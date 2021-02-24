f8cbceb10a219f8fb4b79f205736736ab2254701
Shem <shemnjiir@gmail.com>
2021-02-24 11:56:35 +0300

    addedhtml, css and javascriptfiles

---
+++ popup.js
@@ -0,0 +1,33 @@
+
+window.addEventListener("DOMContentLoaded", function(){
+    console.log("Content loaded");
+
+
+    let form = document.querySelector("#form");
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+        console.log("FORM SUBMITTED");
+
+        let userDate = document.querySelector("#date").value;
+        let date = new Date(userDate);
+        let dateDay = date.getDay();
+        userDate = dateDay;
+        console.log(dateDay);
+
+        let gender = document.querySelector("#gender").value;
+        let male = ['Kwasi','Kwadwo', 'Kwabena', 'Kwako', 'Yaw', 'Kofi', 'Kwame']
+        let female = ['Akoswa', 'Adwoa', 'Abenaa', 'Akua', 'Yua', 'Afua', 'Ama']
+
+        if(gender === "female"){
+            alert(female[userDate]);
+        }
+        else if(gender === "male"){
+            alert(male[userDate]);
+        }
+        else{
+          alert ("Invalid Option") 
+        }
+
+    });
+});
+var modal = document.getElementById('popup');
